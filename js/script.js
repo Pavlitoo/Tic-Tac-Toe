36b0cd4aa8167dd404c26555b70af44d667175f8
Pavlo Lugovy <132191934+Pavlitoo@users.noreply.github.com>
2024-01-11 22:07:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,113 +1,80 @@
-var counter = 0;
-var xWins = 0;
-var oWins = 0;
-var cells = document.querySelectorAll('#field td');
-var header = document.getElementById('header');
-var scoreDisplay = document.getElementById('score');
+let boxes = document.querySelectorAll(".box");
 
-function updateScore() {
-    scoreDisplay.innerText = 'X: ' + xWins + ' | O: ' + oWins;
+let turn = "X";
+let isGameOver = false;
+
+boxes.forEach(e =>{
+    e.innerHTML = ""
+    e.addEventListener("click", ()=>{
+        if(!isGameOver && e.innerHTML === ""){
+            e.innerHTML = turn;
+            cheakWin();
+            cheakDraw();
+            changeTurn();
+        }
+    })
+})
+
+function changeTurn(){
+    if(turn === "X"){
+        turn = "O";
+        document.querySelector(".bg").style.left = "85px";
+    }
+    else{
+        turn = "X";
+        document.querySelector(".bg").style.left = "0";
+    }
 }
 
-function isVictory() {
-    var combos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-    for (var combo of combos) {
-        if (cells[combo[0]].innerHTML == cells[combo[1]].innerHTML && cells[combo[1]].innerHTML == cells[combo[2]].innerHTML && cells[combo[0]].innerHTML != '') {
-            return true;
+function cheakWin(){
+    let winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for(let i = 0; i<winConditions.length; i++){
+        let v0 = boxes[winConditions[i][0]].innerHTML;
+        let v1 = boxes[winConditions[i][1]].innerHTML;
+        let v2 = boxes[winConditions[i][2]].innerHTML;
+
+        if(v0 != "" && v0 === v1 && v0 === v2){
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = turn + " win";
+            document.querySelector("#play-again").style.display = "inline"
+
+            for(j = 0; j<3; j++){
+                boxes[winConditions[i][j]].style.backgroundColor = "#196cb9"
+                boxes[winConditions[i][j]].style.color = "#000"
+            }
         }
     }
-    return false;
 }
 
-function endGame() {
-    for (var cell of cells) {
-        cell.removeEventListener('click', tap);
-    }
+function cheakDraw(){
+    if(!isGameOver){
+        let isDraw = true;
+        boxes.forEach(e =>{
+            if(e.innerHTML === "") isDraw = false;
+        })
 
-    setTimeout(function () {
-        header.innerText = 'Tic Tac Toe';
-        clearBoard();
-    }, 2000);
-
-    updateScore();
-
-    if (xWins >= 5 || oWins >= 5) {
-        congratulateWinner();
-    }
-}
-
-function congratulateWinner() {
-    var winner = xWins >= 5 ? 'X' : 'O';
-    header.innerText = winner + ' wins the round!';
-    playCelebrationAnimations(); 
-}
-
-function playCelebrationAnimations() {
-    document.body.classList.add('celebration');
-    setTimeout(function () {
-        document.body.classList.remove('celebration');
-        startNewRound();
-    }, 3000); 
-}
-
-function startNewRound() {
-    xWins = 0;
-    oWins = 0;
-    updateScore();
-    startGame();
-}
-
-
-
-function tap(event) {
-    if (counter % 2 == 0) {
-        event.target.innerHTML = '<img src="img/close.png" width=100>';
-    }
-    else {
-        event.target.innerHTML = '<img src="img/circle.png" width=100>';
-    }
-
-    if (isVictory()) {
-        if (counter % 2 == 0) {
-            header.innerText = 'X is winner!';
-            xWins++;
-        } else {
-            header.innerText = 'O is winner!';
-            oWins++;
+        if(isDraw){
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = "Draw";
+            document.querySelector("#play-again").style.display = "inline"
         }
-        endGame();
-    } else if (counter == 8) {
-        header.innerText = 'Draw!';
-        endGame();
-    }
-
-    counter++;
-    event.target.removeEventListener('click', tap);
-}
-
-
-function clearBoard() {
-    for (var cell of cells) {
-        cell.innerHTML = '';
-        cell.addEventListener('click', tap);
     }
 }
 
-function startGame() {
-    header.innerText = 'Tic Tac Toe';
-    counter = 0;
-    clearBoard();
-    updateScore();
-}
+document.querySelector("#play-again").addEventListener("click", ()=>{
+    isGameOver = false;
+    turn = "X";
+    document.querySelector(".bg").style.left = "0";
+    document.querySelector("#results").innerHTML = "";
+    document.querySelector("#play-again").style.display = "none";
 
-
-startGame()
+    boxes.forEach(e =>{
+        e.innerHTML = "";
+        e.style.removeProperty("background-color");
+        e.style.color = "#fff"
+    })
+})
